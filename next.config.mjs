059f509078b0f4dfb69216fd2754f697df77bc2e
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2025-04-09 07:25:06 +0500

    eslint was deleted

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        'apple-market.ru',
-        'fone4u.ie',
-        'static.sello.uz',
-        'media.flixcar.com',
-        'fonez.ie',
-        'tele2.ee',
-        'storage.googleapis.com',
-        'static.insales-cdn.com',
-        'image.oppo.com',
-        'sonyimages.blob.core.windows.net',
-        'uk.store.asus.com',
-        'spb-katalog.ru',
-        's7d1.scene7.com',
-        'www.videotron.com',
-        'www.manua.ls',
-        'asia-exstatic-vivofs.vivo.com',
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: [
+      'apple-market.ru',
+      'fone4u.ie',
+      'static.sello.uz',
+      'media.flixcar.com',
+      'fonez.ie',
+      'tele2.ee',
+      'storage.googleapis.com',
+      'static.insales-cdn.com',
+      'image.oppo.com',
+      'sonyimages.blob.core.windows.net',
+      'uk.store.asus.com',
+      'spb-katalog.ru',
+      's7d1.scene7.com',
+      'www.videotron.com',
+      'www.manua.ls',
+      'asia-exstatic-vivofs.vivo.com',
+    ],
+  },
+  eslint: {
+    // Eslint-ni o'chirib qo'yish
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
